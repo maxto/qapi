989283cf8bdbeb26272fe26d93a130f87648d92e
Max Todaro <m.todaro.ge@gmail.com>
2021-01-27 21:12:33 +0100

    new funs

--- R/quants.R
+++ R/quants.R
@@ -26,16 +26,18 @@ rand_ts <- function(m = 20,n = 1,as_ret = F) {
 
 #' Annualized return
 #'
-#' @param x asset/portfolio returns
-#' @param t frequencey of data. 1: yearly, 4: quarterly, 12: monthly, 52: weekly, 252: daily
+#' Average annualized returns
+#'
+#' @param x asset returns
+#' @param t frequency of data. 1: yearly, 4: quarterly, 12: monthly, 52: weekly, 252: daily
 #' @param method "geometric" or "simple" (def: "geometric")
 #'
 #' @return numeric
 #' @export
 #'
 #' @examples
-#' a <- c(0.003,0.026,0.015,-0.009,0.014,0.024,0.015,0.066,-0.014,0.039)
-#' ann_return(a,t=12,method="geometric")
+#' xx <- c(0.003,0.026,0.015,-0.009,0.014,0.024,0.015,0.066,-0.014,0.039)
+#' ann_return(xx,t=12,method="geometric")
 #'
 ann_return <- function(x,t=252,method="geometric") {
   n <- length(x)
@@ -45,3 +47,107 @@ ann_return <- function(x,t=252,method="geometric") {
          stop("unkown method")
   )
 }
+
+#' Annualized Risk
+#'
+#' Annualized standard deviation
+#'
+#' @param x asset returns
+#' @param t frequency of data. 1: yearly, 4: quarterly, 12: monthly, 52: weekly, 252: daily
+#'
+#' @return numeric
+#' @export
+#'
+#' @examples
+#' xx <- c(0.003,0.026,0.015,-0.009,0.014,0.024,0.015,0.066,-0.014,0.039)
+#' ann_risk(xx,t=12)
+#'
+ann_risk <- function(x,t=252) {
+  stats::sd(x) * sqrt(t)
+}
+
+
+
+#' Active return
+#'
+#' Asset annualized return minus Benchmark annualized return
+#'
+#' @param x asset returns
+#' @param y benchmark returns
+#' @param t frequency of data. 1: yearly, 4: quarterly, 12: monthly, 52: weekly, 252: daily
+#' @param method "geometric" or "simple" (def: "geometric")
+#'
+#' @return numeric
+#' @export
+#'
+#' @examples
+#' xx <- c(0.003,0.026,0.015,-0.009,0.014,0.024,0.015,0.066,-0.014,0.039)
+#' yy <- c(0.04,-0.022,0.043,0.028,-0.078,-0.011,0.033,-0.049,0.09,0.087)
+#' active_return(xx,yy,t=12)
+#'
+active_return <- function(x,y,t=252,method="geometric") {
+  ann_return(x,t,method) - ann_return(y,t,method)
+
+}
+
+
+#' Sharpe ratio
+#'
+#' Sharpe ratio for a vector of asset/portolio returns
+#'
+#' @param x asset returns
+#' @param frisk annual free-risk rate (def: 0). For daily rate (frisk/252), weekly (frisk/52), monthly (frisk/12), etc
+#'
+#' @return numeric
+#' @export
+#'
+#' @examples
+#' xx <- c(0.003,0.026,0.015,-0.009,0.014,0.024,0.015,0.066,-0.014,0.039)
+#' sharpe_ratio(xx,frisk=0.02/12)
+#'
+sharpe_ratio <- function(x,frisk=0) {
+  (mean(x) - frisk)/stats::sd(x)
+}
+
+
+#' Excess return
+#'
+#' Return on asset - risk free rate
+#'
+#' @param x asset returns
+#' @param frisk annual free-risk rate (def: 0). For daily rate (frisk/252), weekly (frisk/52), monthly (frisk/12), etc
+#'
+#' @return numeric
+#' @export
+#'
+#' @examples
+#' xx <- c(0.003,0.026,0.015,-0.009,0.014,0.024,0.015,0.066,-0.014,0.039)
+#' excess_return(xx,frisk=0.02/12)
+#'
+excess_return <- function(x,frisk=0) {
+  x - rep(frisk,1,length(x))
+}
+
+
+#' Annualized Sharpe ratio
+#'
+#' Sharpe ratio calculated on annualized excess return / annualized standard deviation
+#'
+#' @param x asset returns
+#' @param frisk annual free-risk rate (def: 0). For daily rate (frisk/252), weekly (frisk/52), monthly (frisk/12), etc
+#' @param t frequency of data. 1: yearly, 4: quarterly, 12: monthly, 52: weekly, 252: daily
+#' @param method method "geometric" or "simple" (def: "geometric")
+#'
+#' @return numeric
+#' @export
+#'
+#' @examples
+#' xx <- c(0.003,0.026,0.015,-0.009,0.014,0.024,0.015,0.066,-0.014,0.039)
+#' ann_sharpe_ratio(xx,frisk=0.02/12,t=12,method="geometric")
+#'
+ann_sharpe_ratio <- function(x,frisk=0,t=252,method="geometric") {
+  xs <- excess_return(x,frisk)
+  ann_return(xs,t,method)/ann_risk(x,t)
+}
+
+
--- tests/testthat/test-quants.R
+++ tests/testthat/test-quants.R
@@ -1,12 +1,44 @@
-xx <- rand_ts(m = 20,n = 2)
+A <- rand_ts(m = 20,n = 2,as_ret = T)
+xx <- c(0.003,0.026,0.015,-0.009,0.014,0.024,0.015,0.066,-0.014,0.039)
+yy <- c(0.04,-0.022,0.043,0.028,-0.078,-0.011,0.033,-0.049,0.09,0.087)
 
 test_that("rand_ts", {
-  expect_type(xx,"double")
-  expect_equal(dim(xx),c(20,2))
+  expect_type(A,"double")
+  expect_equal(dim(A),c(20,2))
 })
 
 test_that("ann_return",{
-  a <- c(0.003,0.026,0.015,-0.009,0.014,0.024,0.015,0.066,-0.014,0.039)
-  expect_equal(ann_return(a,t=12,method="geometric"),0.2338147,tolerance = 1e-7)
+  expect_equal(ann_return(xx,t=12,method="geometric"),0.2338147,tolerance = 1e-7)
   expect_type(ann_return(xx),"double")
+  expect_error(ann_return(xx,method = "armonic"))
+})
+
+
+test_that("ann_risk",{
+  expect_equal(ann_risk(xx,t=12),0.08047277,tolerance = 1e-7)
+  expect_type(ann_risk(xx),"double")
+})
+
+test_that("active_return",{
+  expect_equal(active_return(xx,yy,t=12),0.04197881,tolerance = 1e-7)
+  expect_type(ann_risk(xx),"double")
+  expect_type(ann_risk(yy),"double")
+  expect_length(active_return(A[,1],A[,2]),1L)
+})
+
+test_that("sharpe_ratio",{
+  expect_equal(sharpe_ratio(xx),0.7705391,tolerance = 1e-7)
+  expect_type(sharpe_ratio(xx),"double")
+})
+
+test_that("excess_return",{
+  res <- c(0.001333333,0.024333333,0.013333333,-0.010666667,0.012333333,0.022333333,0.013333333,0.064333333,-0.015666667,0.037333333)
+  expect_equal(excess_return(xx,frisk = 0.02/12),res,tolerance = 1e-7)
+  expect_type(excess_return(xx),"double")
+  expect_length(excess_return(xx,frisk = 0.02/12),length(res))
+})
+
+test_that("ann_sharpe_ratio",{
+  expect_equal(ann_sharpe_ratio(xx,t = 12),2.905513,tolerance = 1e-7)
+  expect_type(ann_sharpe_ratio(xx),"double")
 })
